e30b27d90d790d73a9029020bd468aa53b69ef07
Igor <105680110+Igor-Mateus-da-Silva@users.noreply.github.com>
2024-10-03 13:17:08 -0300

    ajuste na `produto.controller`

--- essencial/backend/src/produto/produto.controller.ts
+++ essencial/backend/src/produto/produto.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { Produto, produtos } from 'src/core';
 
 @Controller('produtos')
 export class ProdutoController {
   @Get()
-  obterProdutos(): Produto[] {
+  async obterProdutos(): Promise<Produto[]> {
     return produtos.map((produto) => ({
       ...produto,
       especificacoes: { destaque: produto.especificacoes.destaque },
     }));
+  }
+
+  @Get(':id')
+  async obterProdutoPorId(@Param('id') id: string): Promise<Produto | null> {
+    const produto = produtos.find((produto) => produto.id === +id);
+    return produto ?? null;
   }
 }
